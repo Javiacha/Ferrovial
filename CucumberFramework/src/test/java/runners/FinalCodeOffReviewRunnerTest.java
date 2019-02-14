732d39df11bede0bdb9f9edf77e68a9e3bebac9a
franciscojavier.acha <franciscojavier.acha@ES-L001992.groupinfra.com>
2019-02-14 09:27:29 +0100

    commit

--- CucumberFramework/src/test/java/runners/FinalCodeOffReviewRunnerTest.java
+++ CucumberFramework/src/test/java/runners/FinalCodeOffReviewRunnerTest.java
@@ -18,7 +18,7 @@ import pageObjects.BasePage;
 				plugin = { "pretty", "html:target/cucumber", "json:target/cucumber.json",
 				"com.cucumber.listener.ExtentCucumberFormatter:output/report.html" })
 
-public class FinalCodeOffReviewRunner extends AbstractTestNGCucumberTests {
+public class FinalCodeOffReviewRunnerTest extends AbstractTestNGCucumberTests {
 	@AfterClass
 	public static void writeExtentReport() throws IOException {
 		Reporter.loadXMLConfig(new File(System.getProperty("user.dir") + "\\src\\main\\java\\utils\\ReportsConfig.xml"));
--- CucumberFramework/src/test/java/runners/JournalVoucherMonitoringRunner.java
+++ CucumberFramework/src/test/java/runners/JournalVoucherMonitoringRunner.java
@@ -18,7 +18,7 @@ import pageObjects.BasePage;
 				plugin = { "pretty", "html:target/cucumber", "json:target/cucumber.json",
 				"com.cucumber.listener.ExtentCucumberFormatter:output/report.html" })
 
-public class JournalVoucherMonitoringRunnerTest extends AbstractTestNGCucumberTests {
+public class JournalVoucherMonitoringRunner extends AbstractTestNGCucumberTests {
 	@AfterClass
 	public static void writeExtentReport() throws IOException {
 		Reporter.loadXMLConfig(new File(System.getProperty("user.dir") + "\\src\\main\\java\\utils\\ReportsConfig.xml"));
