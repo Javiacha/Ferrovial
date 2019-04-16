8560f8333770021a12123175ba5b52b21bebf8ce
franciscojavier.acha <franciscojavier.acha@ES-L001992.groupinfra.com>
2019-04-16 15:21:42 +0200

    commit

--- CucumberFramework/src/test/java/resources/runners/TSMmodemanagerrunnerTest.java
+++ CucumberFramework/src/test/java/resources/runners/TSMmodemanagerrunnerTest.java
@@ -18,7 +18,7 @@ glue = {"stepDefinitions\\TsmModeManagementTest"}, monochrome = true, tags = {},
 				plugin = { "pretty", "html:target/cucumber", "json:target/cucumber.json",
 				"com.cucumber.listener.ExtentCucumberFormatter:output/report.html" })
 
-public class TSMmodemanagerTest extends AbstractTestNGCucumberTests {
+public class TSMmodemanagerrunnerTest extends AbstractTestNGCucumberTests {
 	@AfterClass
 	public static void writeExtentReport() throws IOException {
 		Reporter.loadXMLConfig(new File(System.getProperty("user.dir") + "\\src\\main\\java\\utils\\ReportsConfig.xml"));
